bc49a0b81976ba2f5d906c0cb7995723f99c5b61
eusoumiguel <113737239+eusoumiguel@users.noreply.github.com>
2023-04-04 10:48:54 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function take_snapshot(){
 
 console.log('ml5 version:',ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/D4rdHiaYu/model.js',modelLoad);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/_IFLKOMvC/model.js',modelLoad);
 
 function modelLoaded(){
     console.log('Model Loaded!');
